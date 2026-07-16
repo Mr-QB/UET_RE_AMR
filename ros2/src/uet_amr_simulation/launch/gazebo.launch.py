56c8cd325c0cc4093e79c4559de3ce4b6076286f
dabgxd <loldabg0d2k@gmail.com>
2026-07-16 17:26:21 +0700

    move launch files to uet_amr_simulation

--- ros2/src/uet_amr_simulation/launch/gazebo.launch.py
+++ ros2/src/uet_amr_simulation/launch/gazebo.launch.py
@@ -1,73 +1,83 @@
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
 import os
-import xacro
 from ament_index_python.packages import get_package_share_directory
-
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import xacro
+from os.path import join
 
 def generate_launch_description():
-    share_dir = get_package_share_directory('uet_amr_description')
 
-    xacro_file = os.path.join(share_dir, 'urdf', 'uet_amr.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
-    robot_urdf = robot_description_config.toxml()
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    pkg_ros_gz_rbot = get_package_share_directory('uet_amr_description')
+    pkg_uet_amr_sim = get_package_share_directory('uet_amr_simulation')
 
-    robot_state_publisher_node = Node(
+
+    robot_description_file = os.path.join(pkg_ros_gz_rbot, 'urdf', 'uet_amr.xacro')
+    ros_gz_bridge_config = os.path.join(pkg_ros_gz_rbot, 'config', 'ros_gz_bridge_gazebo.yaml')
+    world_file = os.path.join(pkg_uet_amr_sim, 'worlds', 'warehouse.sdf')
+    
+    robot_description_config = xacro.process_file(robot_description_file)
+    robot_description = {'robot_description': robot_description_config.toxml()}
+
+    load_joint_state_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    load_diff_drive_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_controller", "--controller-manager", "/controller_manager"],
+    )
+
+   
+    robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
-        parameters=[
-            {'robot_description': robot_urdf}
-        ]
+        output='screen',
+        parameters=[robot_description],
     )
 
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher'
+   
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")),
+        launch_arguments={"gz_args": f"-r -v 4 {world_file}"}.items()
     )
 
-    gazebo_server = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gazebo_ros'),
-                'launch',
-                'gzserver.launch.py'
-            ])
-        ]),
-        launch_arguments={
-            'pause': 'true'
-        }.items()
+    spawn_robot = TimerAction(
+        period=5.0,  
+        actions=[Node(
+            package='ros_gz_sim',
+            executable='create',
+            arguments=[
+                "-topic", "/robot_description",
+                "-name", "uet_amr",
+                "-allow_renaming", "false",  
+                "-x", "0.0",
+                "-y", "0.0",
+                "-z", "0.32",
+                "-Y", "0.0"
+            ],
+            output='screen'
+        )]
     )
 
-    gazebo_client = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gazebo_ros'),
-                'launch',
-                'gzclient.launch.py'
-            ])
-        ])
-    )
-
-    urdf_spawn_node = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        arguments=[
-            '-entity', 'AGV_Robot_Model',
-            '-topic', 'robot_description'
-        ],
+    ros_gz_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[{'config_file': ros_gz_bridge_config}],
         output='screen'
     )
 
     return LaunchDescription([
-        robot_state_publisher_node,
-        joint_state_publisher_node,
-        gazebo_server,
-        gazebo_client,
-        urdf_spawn_node,
+        gazebo,
+        spawn_robot,
+        ros_gz_bridge,
+        robot_state_publisher,
+        load_joint_state_broadcaster, 
+        load_diff_drive_controller,   
     ])
