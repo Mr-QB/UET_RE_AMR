b7e8b0e3de682456f28f29187548f5b74214b5df
dabgxd <loldabg0d2k@gmail.com>
2026-08-05 16:32:18 +0700

    update urdf files

--- ros2/src/uet_amr_description/setup.py
+++ ros2/src/uet_amr_description/setup.py
@@ -7,16 +7,15 @@ package_name = 'uet_amr_description'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name], 
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
         (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
-        (os.path.join('share', package_name, 'config'), glob('config/*')),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
